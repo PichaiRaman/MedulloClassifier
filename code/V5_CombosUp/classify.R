83895d73162ce7727fe708a03d88c93584a6944f
arifs1 <arifs1@email.chop.edu>
2019-08-05 15:53:09 -0400

    initialized classify.R function

---
+++ code/V5_CombosUp/classify.R
@@ -0,0 +1,15 @@
+################################
+#Purpose: Classify Medullo Samples
+#Author: Sherjeel Arif
+#Date: 8/5/2019
+################################
+
+#######################
+# Required Libraries
+#######################
+
+library("pheatmap")
+library("GSVA");
+library("caret")
+library("preprocessCore");
+source("calcScore.R")
